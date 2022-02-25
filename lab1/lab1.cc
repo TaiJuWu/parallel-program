6f2900717fe6a6f916626423bc8145703beb2e7a
tjwu <tjwu1217@gmail.com>
2022-02-26 02:23:10 +0800

    Use clang-format to format code

--- lab1/lab1.cc
+++ lab1/lab1.cc
@@ -1,51 +1,53 @@
 #include <assert.h>
-#include <stdio.h>
 #include <math.h>
 #include <mpi.h>
+#include <stdio.h>
 
-int main(int argc, char** argv) {
-	if (argc != 3) {
-		fprintf(stderr, "must provide exactly 2 arguments!\n");
-		return 1;
-	}
-	int rank, size;
-	MPI_Init(&argc, &argv);
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    fprintf(stderr, "must provide exactly 2 arguments!\n");
+    return 1;
+  }
+  int rank, size;
+  MPI_Init(&argc, &argv);
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	unsigned long long r = atoll(argv[1]);
-	unsigned long long k = atoll(argv[2]);
-	unsigned long long task = r / size;
-	if (r % size != 0)
-		task += 1;
-//	unsigned long long task = ceil(float(r)/size);
+  unsigned long long r = atoll(argv[1]);
+  unsigned long long k = atoll(argv[2]);
+  unsigned long long task = r / size;
+  if (r % size != 0)
+    task += 1;
+  //	unsigned long long task = ceil(float(r)/size);
 
-//	if(rank == 0){
-//		printf("task is %d\n", task);
-//		printf("r is %d\n", r);
-//		printf("k is %d\n",k);
-//	}
+  //	if(rank == 0){
+  //		printf("task is %d\n", task);
+  //		printf("r is %d\n", r);
+  //		printf("k is %d\n",k);
+  //	}
 
-	unsigned long long pixels = 0;
-	for (unsigned long long x = rank*task; x < (rank+1)*task && x<r; x++) {
-		unsigned long long y = ceil(sqrtl(r*r - x*x));
-//		printf("rank is %d, x is %d, y is %d\n", rank, x, y); 
-		pixels += y;
-		pixels %= k;
-	}
-	unsigned long long global_pixels;
+  unsigned long long pixels = 0;
+  for (unsigned long long x = rank * task; x < (rank + 1) * task && x < r;
+       x++) {
+    unsigned long long y = ceil(sqrtl(r * r - x * x));
+    //		printf("rank is %d, x is %d, y is %d\n", rank, x, y);
+    pixels += y;
+    pixels %= k;
+  }
+  unsigned long long global_pixels;
 
-	MPI_Barrier(MPI_COMM_WORLD);
-//	printf("rank is %d ,pixels is %llu\n", rank,pixels);
-	MPI_Reduce(&pixels, &global_pixels, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-//	printf("rank is %d, global is %llu\n", rank,global_pixels);
+  MPI_Barrier(MPI_COMM_WORLD);
+  //	printf("rank is %d ,pixels is %llu\n", rank,pixels);
+  MPI_Reduce(&pixels, &global_pixels, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0,
+             MPI_COMM_WORLD);
+  //	printf("rank is %d, global is %llu\n", rank,global_pixels);
 
-	if (rank == 0){
-//		printf("task is %llu\n", task);
-		printf("%llu\n", (4 * global_pixels) % k);
-	}
+  if (rank == 0) {
+    //		printf("task is %llu\n", task);
+    printf("%llu\n", (4 * global_pixels) % k);
+  }
 
-	MPI_Finalize();
-//	printf("size of long long is %d\n", sizeof(long long));
+  MPI_Finalize();
+  //	printf("size of long long is %d\n", sizeof(long long));
 }
--- lab3/lab3_pthread.cc
+++ lab3/lab3_pthread.cc
@@ -1,102 +1,99 @@
 #include <assert.h>
-#include <stdio.h>
+#include <iostream>
 #include <math.h>
 #include <pthread.h>
-#include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
-typedef struct info{
-//	unsigned long long x;
-	unsigned long long k;
-	unsigned long long r;
-	unsigned long long task;
-	unsigned long long result;
-	int threadID;
-	int *t;
-}Cal_info;
+typedef struct info {
+  //	unsigned long long x;
+  unsigned long long k;
+  unsigned long long r;
+  unsigned long long task;
+  unsigned long long result;
+  int threadID;
+  int *t;
+} Cal_info;
 
-void test(int i){
-	printf("hello this is %d\n", i);
+void test(int i) { printf("hello this is %d\n", i); }
+
+void *cal_func(void *info) {
+  Cal_info *ptr = (Cal_info *)info;
+  unsigned long long pixels = 0;
+  unsigned long long r = ptr->r;
+  unsigned long long k = ptr->k;
+  unsigned long long task = ptr->task;
+  int rank = ptr->threadID;
+  ptr->t[rank] = rank + 10;
+
+  for (unsigned long long x = rank * task; x < (rank + 1) * task && x < r;
+       x++) {
+    unsigned long long y = ceil(sqrtl(r * r - x * x));
+    pixels += y;
+  }
+
+  pixels %= k;
+  ptr->result = pixels;
+  test(rank);
+  //	printf("threadID: %d, pixels:%d\n", rank, pixels);
+  return (void *)0;
 }
 
-void *cal_func(void *info){
-	Cal_info* ptr = (Cal_info *)info;
-	unsigned long long pixels = 0;
-	unsigned long long r = ptr->r;
-	unsigned long long k = ptr->k;
-	unsigned long long task = ptr->task;
-	int rank = ptr->threadID;
-	ptr->t[rank] = rank+10;
-	
-	for ( unsigned long long x = rank*task; x<(rank+1)*task && x<r; x++){
-		unsigned long long y = ceil(sqrtl(r*r - x*x));
-		pixels += y;
-	}
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    fprintf(stderr, "must provide exactly 2 arguments!\n");
+    return 1;
+  }
+  unsigned long long r = atoll(argv[1]);
+  unsigned long long k = atoll(argv[2]);
+  unsigned long long pixels = 0;
 
-	pixels %= k;
-	ptr->result = pixels;
-	test(rank);
-//	printf("threadID: %d, pixels:%d\n", rank, pixels);
-	return (void *)0;
-}
+  cpu_set_t cpuset;
+  sched_getaffinity(0, sizeof(cpuset), &cpuset);
+  unsigned long long ncpus = CPU_COUNT(&cpuset);
+  ncpus += 1;
 
+  unsigned long long task = r / ncpus;
+  if (r % ncpus != 0) {
+    ++task;
+  }
+  Cal_info data[ncpus];
+  pthread_t threads[ncpus];
+  // int *t = new int[ncpus];
+  int *t = (int *)malloc(ncpus * sizeof(int));
 
+  // creat child thread calculate
+  for (unsigned long long x = 1; x < ncpus; x++) {
+    data[x].task = task;
+    data[x].threadID = x;
+    data[x].r = r;
+    data[x].k = k;
+    data[x].t = t;
+    pthread_create(&threads[x], NULL, cal_func, (void *)&data[x]);
+  }
 
-int main(int argc, char** argv) {
-	if (argc != 3) {
-		fprintf(stderr, "must provide exactly 2 arguments!\n");
-		return 1;
-	}
-	unsigned long long r = atoll(argv[1]);
-	unsigned long long k = atoll(argv[2]);
-	unsigned long long pixels = 0;
+  // main thread calculate
+  int rank = 0;
+  for (unsigned long long x = rank * task; x < (rank + 1) * task && x < r;
+       x++) {
+    unsigned long long y = ceil(sqrtl(r * r - x * x));
+    pixels += y;
+    //		printf("threadID: %d, pixels:%d\n", rank, pixels);
+  }
 
-	cpu_set_t cpuset;
-	sched_getaffinity(0, sizeof(cpuset), &cpuset);
-	unsigned long long ncpus = CPU_COUNT(&cpuset);
-	ncpus += 1;
-	
-	unsigned long long task = r / ncpus;
-	if ( r%ncpus != 0 ){
-		++task;
-	}
-	Cal_info data[ncpus];
-	pthread_t threads[ncpus];
-	// int *t = new int[ncpus];
-	int *t = (int *)malloc(ncpus * sizeof(int));
+  pixels %= k;
+  // wait all child thread
+  for (unsigned long long x = 1; x < ncpus; x++) {
+    pthread_join(threads[x], NULL);
+    cout << "rank  " << x << " " << data[x].t[x] << endl;
+  }
 
-	// creat child thread calculate
-	for (unsigned long long x = 1; x < ncpus; x++) {
-		data[x].task = task;
-		data[x].threadID = x;
-		data[x].r = r;
-		data[x].k = k;
-		data[x].t = t;
-		pthread_create(&threads[x], NULL, cal_func, (void *)&data[x]);
-	}
-
-	// main thread calculate
-	int rank = 0;
-	for ( unsigned long long x = rank*task; x<(rank+1)*task && x<r; x++){
-		unsigned long long y = ceil(sqrtl(r*r - x*x));
-		pixels += y;
-//		printf("threadID: %d, pixels:%d\n", rank, pixels);	
-	}
-
-	pixels %= k;
-	// wait all child thread
-	for( unsigned long long x = 1; x < ncpus; x++){
-		pthread_join(threads[x], NULL);
-		cout << "rank  " << x << " "<< data[x].t[x] << endl;
-	}
-
-
-	// cummculate sum
-	for(int x=1; x<ncpus; ++x){
-		pixels += data[x].result;
-//		pixels %= k;
-	}
-	pixels %= k;
-	printf("%llu\n", (4 * pixels) % k);
+  // cummculate sum
+  for (int x = 1; x < ncpus; ++x) {
+    pixels += data[x].result;
+    //		pixels %= k;
+  }
+  pixels %= k;
+  printf("%llu\n", (4 * pixels) % k);
 }
